173b7a85fea31931346a43fcaa2731f70a39fe4d
ChanceXML <joseguest1337@gmail.com>
2026-03-10 15:47:18 -0700

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -71,12 +71,16 @@ class Main extends Sprite
 			
 		instance = this;
 
-		#if android
-        if (!Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE)) {
-        Permissions.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
-        }
-        #end
+        #if android
+        var permissions = ["android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"];
 
+        if (cast(lime.system.System.platformVersion, Int) >= 30) {
+        permissions.push("android.permission.MANAGE_EXTERNAL_STORAGE");
+        }
+
+        Permissions.requestPermissions(permissions);
+        #end
+	 
 		CrashHandler.init();
 
 		addChild(game = new FunkinGame(gameWidth, gameHeight, MainState, Options.framerate, Options.framerate, skipSplash, startFullscreen));
