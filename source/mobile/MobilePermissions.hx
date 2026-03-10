3afca359207171e654e4eb0994feea9d5bca4b86
ChanceXML <joseguest1337@gmail.com>
2026-03-10 16:59:02 -0700

    Update MobilePermissions.hx

--- source/mobile/MobilePermissions.hx
+++ source/mobile/MobilePermissions.hx
@@ -1,8 +1,38 @@
 package mobile;
 
-class StorageUtil
+#if android
+import extension.androidtools.os.Build;
+import extension.androidtools.os.Build.VERSION_CODES;
+import extension.androidtools.Permissions;
+import extension.androidtools.os.Environment;
+import extension.androidtools.Settings;
+
+class MobilePermissions
 {
-	public static function requestPermissions():Void
-	{
-	}
+    public static function request():Void
+    {
+        var perms:Array<String>;
+
+        if (Build.SDK_INT >= VERSION_CODES.TIRAMISU) {
+            perms = [
+                "android.permission.READ_MEDIA_IMAGES",
+                "android.permission.READ_MEDIA_VIDEO",
+                "android.permission.READ_MEDIA_AUDIO"
+            ];
+        } else {
+            perms = [
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE"
+            ];
+        }
+
+        Permissions.requestPermissions(perms);
+
+        if (Build.SDK_INT >= VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Settings.requestSetting("android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+            }
+        }
+    }
 }
+#end
